545ab397106f3efc1d3f24fcdf7f066cb7083cb4
wang <wangmingzhang@outlook.com>
2019-03-12 20:57:09 +0800

    修改代码，即使出现异常也保存

--- IJB-A - analyse.py
+++ IJB-A - analyse.py
@@ -5,6 +5,7 @@ import shutil
 import cv2
 import glob
 import csv
+import traceback
 from config import *
 from lighting_estimation import which_direction, gray_level, Statistic
 from SfSNet.sfsnet import SfSNet
@@ -79,71 +80,74 @@ def ijb_a(show=False):
     dir_level_keys = ['%s_%d' % (_d, _l) for _d in direction_keys for _l in level_keys]
     dir_level_sta = Statistic('dir_level.csv', True, *dir_level_keys)
 
-    with open(list_file, mode='r') as f:
-        # 跳过IJB-A列表文件的标题行
-        next(f)
-        # 定义CSV读取器
-        reader = csv.reader(f)
-        # 读取所有的人，并把需要的信息提取出来
-        for line in reader:
-            people_records.append([line[1], line[2],
-                                   (int(float(line[6])), int(float(line[7])),
-                                    int(float(line[8])), int(float(line[9])))])
-        for record in people_records:
-            print '*' * 120
-            print record  # ['417', 'frame/28819_00060.png', (116, 16, 116, 138)]
-            # 从record里读取人脸
-            image, rect = crop_face_from_image(record, show=False)
-            # 对齐和裁剪人脸
-            mask, aligned_image = sfsnet.process_image(image, show=False)
-            if mask is not None:
-                # 对齐成功，mask不为空
-                face, mask, _, _, _, shading = sfsnet.forward(aligned_image, mask)
-            else:
-                # 对齐失败
-                shape = image.shape[0:2]
-                # 调整大小
-                resize_image = cv2.resize(image, (M, M))
-                # 使用未对齐的人脸计算shading
-                face, mask, _, _, _, shading = sfsnet.forward(resize_image, None)
-                # 调整回原来的大小
-                shading = cv2.resize(shading, shape)
-                # 裁剪出人脸
-                shading = shading[rect[0][1]:rect[1][1], rect[0][0]:rect[1][0]]
-                print('shading.shape', shading.shape)
-            # 计算人脸方向和统计梯度的角度
-            direction, angle_count = which_direction(shading, mask, magnitude_threshold=10)
-            angle_count = sorted(angle_count, key=lambda x: x[1], reverse=True)
-            print(direction, angle_count)
+    try:
+        with open(list_file, mode='r') as f:
+            # 跳过IJB-A列表文件的标题行
+            next(f)
+            # 定义CSV读取器
+            reader = csv.reader(f)
+            # 读取所有的人，并把需要的信息提取出来
+            for line in reader:
+                people_records.append([line[1], line[2],
+                                       (int(float(line[6])), int(float(line[7])),
+                                        int(float(line[8])), int(float(line[9])))])
+            for record in people_records:
+                print '*' * 120
+                print record  # ['417', 'frame/28819_00060.png', (116, 16, 116, 138)]
+                # 从record里读取人脸
+                image, rect = crop_face_from_image(record, show=False)
+                # 对齐和裁剪人脸
+                mask, aligned_image = sfsnet.process_image(image, show=False)
+                if mask is not None:
+                    # 对齐成功，mask不为空
+                    face, mask, _, _, _, shading = sfsnet.forward(aligned_image, mask)
+                else:
+                    # 对齐失败
+                    shape = image.shape[0:2]
+                    # 调整大小
+                    resize_image = cv2.resize(image, (M, M))
+                    # 使用未对齐的人脸计算shading
+                    face, mask, _, _, _, shading = sfsnet.forward(resize_image, None)
+                    # 调整回原来的大小
+                    shading = cv2.resize(shading, shape)
+                    # 裁剪出人脸
+                    shading = shading[rect[0][1]:rect[1][1], rect[0][0]:rect[1][0]]
+                    print('shading.shape', shading.shape)
+                # 计算人脸方向和统计梯度的角度
+                direction, angle_count = which_direction(shading, mask, magnitude_threshold=10)
+                angle_count = sorted(angle_count, key=lambda x: x[1], reverse=True)
+                print(direction, angle_count)
 
-            # 计算shading属于那个亮度等级level
-            avg_pixel_val, level = gray_level(shading, mask)
-            print('avg_pixel_val =', avg_pixel_val, 'level =', level)
-            gray_val.append(avg_pixel_val)
+                # 计算shading属于那个亮度等级level
+                avg_pixel_val, level = gray_level(shading, mask)
+                print('avg_pixel_val =', avg_pixel_val, 'level =', level)
+                gray_val.append(avg_pixel_val)
 
-            # 写入统计数据
-            direction_sta.add(record[0], conclude_direction(direction))
-            level_sta.add(record[0], level)
-            dir_level_sta.add(record[0], '%s_%d' % (conclude_direction(direction), level))
+                # 写入统计数据
+                direction_sta.add(record[0], conclude_direction(direction))
+                level_sta.add(record[0], level)
+                dir_level_sta.add(record[0], '%s_%d' % (conclude_direction(direction), level))
 
-            # 默认存储路径
-            id_dir = os.path.join(base_dir, record[0], str(level))
-            # 不存在则新建图像
-            if not os.path.exists(id_dir):
-                os.makedirs(id_dir)
-            # 存储图像
-            cv2.imwrite(os.path.join(id_dir, record[1].split('/')[-1]), face)
-            if show:
-                cv2.imshow('face', face)
-                cv2.imshow('shading', shading)
-                if cv2.waitKey(50) == 27:
-                    print 'Exiting...'
-                    exit()
-        print np.max(gray_val), np.min(gray_val)
+                # 默认存储路径
+                id_dir = os.path.join(base_dir, record[0], str(level))
+                # 不存在则新建图像
+                if not os.path.exists(id_dir):
+                    os.makedirs(id_dir)
+                # 存储图像
+                cv2.imwrite(os.path.join(id_dir, record[1].split('/')[-1]), face)
+                if show:
+                    cv2.imshow('face', face)
+                    cv2.imshow('shading', shading)
+                    if cv2.waitKey(50) == 27:
+                        print 'Exiting...'
+                        exit()
+            print np.max(gray_val), np.min(gray_val)
+    except:
         # 保存统计信息
         direction_sta.save()
         level_sta.save()
         dir_level_sta.save()
+        traceback.print_exc()
 
 
 def conclude_direction(direction):
