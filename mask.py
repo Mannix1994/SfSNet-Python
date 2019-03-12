d4b356c087851328d7b1647963cbbc2cf471baba
wang <wangmingzhang@outlook.com>
2019-03-12 13:29:52 +0800

    修复mask.py的bug

--- mask.py
+++ mask.py
@@ -6,12 +6,12 @@ import dlib
 import cv2
 import sys
 import numpy as np
-from PIL import Image
-from functions import create_mask_fiducial
+from SfSNet.functions import create_mask_fiducial
+from config import LANDMARK_PATH
 
 
 class MaskGenerator:
-    def __init__(self, landmarks_path='shape_predictor_68_face_landmarks.dat'):
+    def __init__(self, landmarks_path=LANDMARK_PATH):
         """
         :param landmarks_path: the path of pretrained key points weight,
         it could be download from:
@@ -26,7 +26,14 @@ class MaskGenerator:
         # cv2.waitKey(50)
         return mask & img
 
-    def align(self, image, crop_size=(240, 240), scale=3.5, show_landmarks=False):
+    def bounding_boxes(self, image):
+        # convert to gray image
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # get rect contains face
+        face_rects = self._detector(gray_image, 0)
+        return face_rects
+
+    def align(self, image, crop_size=(240, 240), scale=3.5, show_landmarks=False, return_none=False):
         """
         :param show_landmarks:
         :param scale:
@@ -36,25 +43,30 @@ class MaskGenerator:
         :return: a face image with mask
         https://blog.csdn.net/qq_39438636/article/details/79304130
         """
-        # convert to gray image
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # get rect contains face
-        face_rects = self._detector(gray_image, 0)
+        # get rectangles which contains face
+        face_rects = self.bounding_boxes(image)
         for i in range(len(face_rects)):
             # get 68 landmarks of face
             landmarks = np.array([[p.x, p.y] for p in self._predictor(image, face_rects[i]).parts()])
+            # show landmarks
+            if show_landmarks:
+                self.show_landmarks(image, landmarks)
             # create mask using landmarks
             mask = create_mask_fiducial(landmarks.T, image)
             # warp and crop image
             mask, image = self._warp_and_crop_face(image, mask, landmarks, crop_size, scale)
-            # show landmarks
-            if show_landmarks:
-                self.show_landmarks(image, landmarks)
+            # # detect rectangle
+            # rect = self._detector(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), 0)
+            # cv2.rectangle(mask, (rect[i].left(), rect[i].top()),
+            #               (rect[i].right(), rect[i].bottom()), (255, 255, 0))
             return mask, image,
         else:
             sys.stderr.write("%s: Can't detect face in image\n" % __file__)
             image = cv2.resize(image, crop_size)
-            return np.ones(image.shape) * 255, image
+            if return_none:
+                return None, image
+            else:
+                return np.ones(image.shape, dtype=image.dtype)*255, image
 
     def _warp_and_crop_face(self, image, mask, landmarks, crop_size, scale):
         """
@@ -78,8 +90,9 @@ class MaskGenerator:
         rot_mat = cv2.getRotationMatrix2D(tuple(landmarks[2]), r_angle, scale=1)
 
         # rotate image and mask
-        rotated_image = cv2.warpAffine(image, rot_mat, dsize=image.shape[0:2])
-        rotated_mask = cv2.warpAffine(mask, rot_mat, dsize=image.shape[0:2])
+        ims = image.shape[0:2]
+        rotated_image = cv2.warpAffine(image, rot_mat, dsize=(ims[0]*2, ims[1]*2))
+        rotated_mask = cv2.warpAffine(mask, rot_mat, dsize=(ims[0]*2, ims[1]*2))
 
         # crop image and mask
         cropped_image = self._crop_image(rotated_image, landmarks, scale)
@@ -98,26 +111,34 @@ class MaskGenerator:
         size = distance * scale
         # compute row_start, row_end, col_start, col_end
         landmark_nose = landmarks[30]
-        row_start = int(landmark_nose[1]-size/2)
-        row_end = int(landmark_nose[1]+size/2)
-        col_start = int(landmark_nose[0]-size/2)
-        col_end = int(landmark_nose[0]+size/2)
+        row_start = int(landmark_nose[1]-size/2.0)
+        row_end = int(landmark_nose[1]+size/2.0)
+        col_start = int(landmark_nose[0]-size/2.0)
+        col_end = int(landmark_nose[0]+size/2.0)
+        # print('*' * 10)
+        # print(row_start, row_end, col_start, col_end)
         # make range valid
-        row_start = row_start if row_start > 0 else 0
-        row_end = row_end if row_end < image.shape[0] else image.shape[0]
-        col_start = col_start if col_start > 0 else 0
-        col_end = col_end if col_end < image.shape[1] else image.shape[1]
+        if row_start < 0:
+            row_end += abs(row_start)
+            row_start = 0
+        if col_start < 0:
+            col_end += abs(col_start)
+            col_start = 0
+        row_end = min(row_end, image.shape[0])
+        col_end = min(col_end, image.shape[1])
+        # print(row_start, row_end, col_start, col_end)
+        # print('*' * 10)
         # crop image
         cropped_image = image[row_start:row_end, col_start:col_end]
         rows, cols, _ = cropped_image.shape
         _min = np.min((rows, cols))
         if _min < cols:
             # rows is smaller than cols
-            padding = np.zeros(shape=(cols-_min, cols, 3), dtype=np.uint8)
+            padding = np.zeros(shape=(cols-_min, cols, 3), dtype=cropped_image.dtype)
             cropped_image = np.vstack((cropped_image, padding))
         elif _min < rows:
             # cols is smaller than rows
-            padding = np.zeros(shape=(rows, rows - _min, 3), dtype=np.uint8)
+            padding = np.zeros(shape=(rows, rows - _min, 3), dtype=cropped_image.dtype)
             cropped_image = np.hstack((cropped_image, padding))
         return cropped_image
 
@@ -126,14 +147,15 @@ class MaskGenerator:
         for i, landmark in enumerate(landmarks):
             cv2.circle(im, tuple(landmark), 3, (0, 0, 255))
             cv2.putText(im, str(i), tuple(landmark), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (0, 255, 0))
+                        0.3, (0, 255, 0))
         cv2.imshow("landmarks", im)
         cv2.waitKey(50)
 
 
 if __name__ == '__main__':
-    image = cv2.imread('Images/0003_01.jpg')
-    mask_gen = MaskGenerator()
+    from config import LANDMARK_PATH
+    image = cv2.imread('Images/4.png_face.png')
+    mask_gen = MaskGenerator(LANDMARK_PATH)
     mah= mask_gen.get_masked_face(image)
     cv2.imshow('123', mah)
     cv2.waitKey(0)
